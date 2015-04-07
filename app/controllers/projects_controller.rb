0638b0cbe004b16b0e390607b229f18580593958
Michał Janeczek <michal.janeczek@ymail.com>
2015-04-07 10:45:00 +0200

    Example of crash by invalid data in cookie

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,7 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
+  before_action :store_salary_in_cookies
+  before_action :set_available_project_count, only: :new
 
   # GET /projects
   def index
@@ -46,13 +48,27 @@ class ProjectsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_project
-      @project = Project.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def project_params
-      params.require(:project).permit(:name, :employees_count)
+  # Use callbacks to share common setup or constraints between actions.
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def project_params
+    params.require(:project).permit(:name, :employees_count)
+  end
+
+  def store_salary_in_cookies
+    if params[:salary].present?
+      cookies[:budget] = params[:salary][:budget]
+      cookies[:amount] = params[:salary][:amount]
     end
+  end
+
+  def set_available_project_count
+    if cookies[:budget] && cookies[:amount]
+      params[:available_project_count] = (cookies[:budget].to_i / cookies[:amount].to_i) - Project.count
+    end
+  end
 end
