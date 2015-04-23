2e30961864e738f07f25a85c74e453f775629301
crashbreak-tester <zagubiony@ymail.com>
2015-04-23 14:46:09 +0200

    Add test file for error 27

---
+++ spec/crashbreak_error_spec.rb
@@ -0,0 +1,11 @@
+describe 'error id: 27', type: :request do
+
+  let(:restorers_data) { StateRestorer.new('27').restore }
+  let(:request_parser) { Crashbreak::RequestParser.new restorers_data[:request] }
+
+  it 'sends request' do
+    request_parser.request_data do |request_method, url, body, headers|
+      method(request_method).call url, body, headers
+    end
+  end
+end
