d7cb9e39d6d5359797c3a7ce83571257fd923afa
Michał Janeczek <michal.janeczek@ymail.com>
2015-08-07 21:41:56 +0200

    Init mongodb, add example scaffold

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'unicorn'
 gem 'sidekiq'
+gem 'mongoid', '~> 5.0.0.beta'
 
 group :development, :test do
   gem 'byebug'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :articles
   resources :projects
   resources :projects
   # The priority is based upon order of creation: first created -> highest priority.
